2c6bde31978129bdec2d425fe7b8584a631180f1
Daniel-PeGa <danielperezgarnic@gmail.com>
2023-01-19 22:03:48 -0600

    ya le pude meter foto, ya masomenos se lo que estoy haciendo lol

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function About() {
     return (
+        <div>
         <p>Im Daniel Perez Garnica and this is my about page</p>
+        <img className="profilePicture" src="/images/danielPhoto.jpeg" alt="danielPhoto" />
+        </div>
     );
 }
 
