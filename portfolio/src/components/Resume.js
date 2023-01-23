901acd4c992b1808c06a5ec4303a3badcf9c06af
Daniel-PeGa <danielperezgarnic@gmail.com>
2023-01-22 21:13:55 -0600

    ok minimo ahora ya jala

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Resume() {
     return (
-        <div className="flexCenterRow w125">
+        <div className="flexCenterCol w100">
             <div className="flexCenterCol">
                 <p>Feel free to check my resume on the button below</p>
             <a className="button is-primary text-decorations" href={process.env.PUBLIC_URL + "/danielPerezGarnicaResume.pdf"} target="_blank" rel="noreferrer"
@@ -11,6 +11,15 @@ function Resume() {
 
             <div>
                 <h3>My skills include:</h3>
+                <ul className="noList">
+                    <li>Third-party APIs</li>
+                    <li>MySQL</li>
+                    <li>JavaScript</li>
+                    <li>Python</li>
+                    <li>Advanced CSS</li>
+                    <li>jQuery</li>
+
+                </ul>
             </div>
         </div>
     );
